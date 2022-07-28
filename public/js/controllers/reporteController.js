32c2872314c522f0c62d64247a208d934c78d053
Allan Rodriguez Venegas <arodriguezv@ucenfotec.ac.cr>
2022-07-28 00:00:24 -0600

    Reporte de citas

--- public/js/controllers/reporteController.js
+++ public/js/controllers/reporteController.js
@@ -1,16 +1,25 @@
 'use strict'
 
+var listaCitas;
+
+window.addEventListener('load', () =>{
+     cargaJsonCitas();   
+});
 
 
 function ImprimirListaCitas(){
+
+    listaCitas = getCitasArray();
     let tbody = document.getElementById('tbdCitas');
-
-    let listaCitas = ObtenerListaCitas();
-
+    //console.log(listaCitas);
     tbody.innerHTML = '';
 
     for (let i = 0; i < listaCitas.length; i++) {
+
+      let  cita = listaCitas[i];
         
+        // console.log(cita);
+
         let fila = tbody.insertRow();
         let celdaNumCita = fila.insertCell();
         let celdaMascota = fila.insertCell();
@@ -19,122 +28,123 @@ function ImprimirListaCitas(){
         let celdaEstado = fila.insertCell();
         let celdaBoton = fila.insertCell();
 
-        celdaNumCita.innerHTML = listaCitas[i][1];
-        celdaNumCita.classList.add('infoTd');
-        celdaMascota.innerHTML = listaCitas[i][2];
-        celdaMascota.classList.add('infoTd');
-        celdaVeterinario.innerHTML = listaCitas[i][3];
-        celdaVeterinario.classList.add('infoTd');
-        celdaFecha.innerHTML = listaCitas[i][4];
-        celdaFecha.classList.add('infoTd');
-        celdaEstado.innerHTML = listaCitas[i][5];
-        celdaEstado.classList.add('Estado');
-        celdaEstado.classList.add('infoTd');
+        
+
+         celdaNumCita.innerHTML = cita.NumeroCita;
+         celdaNumCita.classList.add('infoTd');
+         celdaMascota.innerHTML = cita.NombreMascota;
+         celdaMascota.classList.add('infoTd');
+         celdaVeterinario.innerHTML = cita.IdentificacionVeterinario;
+         celdaVeterinario.classList.add('infoTd');
+         celdaFecha.innerHTML = cita.FechaHora;
+         celdaFecha.classList.add('infoTd');
+         celdaEstado.innerHTML = cita.Estado;
+         celdaEstado.classList.add('Estado');
+         celdaEstado.classList.add('infoTd');
         
 
         
     }
-    let EstadoCita = document.querySelectorAll('.Estado');
-        console.log(EstadoCita.length);
-        VerEstado(EstadoCita);
+     let EstadoCita = document.querySelectorAll('.Estado');
+         console.log(EstadoCita.length);
+         VerEstado(EstadoCita);
 }
 
 
 
 
+// function ImprimirListaReservas(){
+//     let tbody = document.getElementById('tbdReservas');
 
-function ImprimirListaReservas(){
-    let tbody = document.getElementById('tbdReservas');
+//     let listaReservas = ObtenerListaReservas();
 
-    let listaReservas = ObtenerListaReservas();
+//     tbody.innerHTML = '';
 
-    tbody.innerHTML = '';
-
-    for (let i = 0; i < listaReservas.length; i++) {
+//     for (let i = 0; i < listaReservas.length; i++) {
         
-        let fila = tbody.insertRow();
-        let celdaNumReserva = fila.insertCell();
-        let celdaMascota = fila.insertCell();
-        let celdafechaEnt = fila.insertCell();
-        let celdaFecha= fila.insertCell();
-        let celdaEstado = fila.insertCell();
-        let celdaBoton = fila.insertCell();
+//         let fila = tbody.insertRow();
+//         let celdaNumReserva = fila.insertCell();
+//         let celdaMascota = fila.insertCell();
+//         let celdafechaEnt = fila.insertCell();
+//         let celdaFecha= fila.insertCell();
+//         let celdaEstado = fila.insertCell();
+//         let celdaBoton = fila.insertCell();
 
-        celdaNumReserva.innerHTML = listaReservas[i][1];
-        celdaNumReserva.classList.add('infoTd');
-        celdaMascota.innerHTML = listaReservas[i][2];
-        celdaMascota.classList.add('infoTd');
-        celdafechaEnt.innerHTML = listaReservas[i][3];
-        celdafechaEnt.classList.add('infoTd');
-        celdaFecha.innerHTML = listaReservas[i][4];
-        celdaFecha.classList.add('infoTd');
-        celdaEstado.innerHTML = listaReservas[i][5];
-        celdaEstado.classList.add('Estado');
-        celdaEstado.classList.add('infoTd');
+//         celdaNumReserva.innerHTML = listaReservas[i][1];
+//         celdaNumReserva.classList.add('infoTd');
+//         celdaMascota.innerHTML = listaReservas[i][2];
+//         celdaMascota.classList.add('infoTd');
+//         celdafechaEnt.innerHTML = listaReservas[i][3];
+//         celdafechaEnt.classList.add('infoTd');
+//         celdaFecha.innerHTML = listaReservas[i][4];
+//         celdaFecha.classList.add('infoTd');
+//         celdaEstado.innerHTML = listaReservas[i][5];
+//         celdaEstado.classList.add('Estado');
+//         celdaEstado.classList.add('infoTd');
         
-        let EstadoCitaif = document.querySelectorAll('.Estado');
-        if (EstadoCitaif[i].innerHTML == 'Pendiente' ) {
-        let BotonV = document.createElement('a');
-        BotonV.setAttribute('href','#')
-        let iconoV =document.createElement('i');
-        iconoV.classList.add("fa-solid")
-        iconoV.classList.add("fa-eye")
-        iconoV.classList.add("btnV")
-        BotonV.appendChild(iconoV);
-        celdaBoton.appendChild(BotonV);
+//         let EstadoCitaif = document.querySelectorAll('.Estado');
+//         if (EstadoCitaif[i].innerHTML == 'Pendiente' ) {
+//         let BotonV = document.createElement('a');
+//         BotonV.setAttribute('href','#')
+//         let iconoV =document.createElement('i');
+//         iconoV.classList.add("fa-solid")
+//         iconoV.classList.add("fa-eye")
+//         iconoV.classList.add("btnV")
+//         BotonV.appendChild(iconoV);
+//         celdaBoton.appendChild(BotonV);
         
-        let Boton = document.createElement('a');
-        Boton.setAttribute('href','#')
-        let icono =document.createElement('i');
-        icono.classList.add("fa-solid")
-        icono.classList.add("fa-pen-to-square")
-        icono.classList.add("btnEd")
-        Boton.appendChild(icono);
-        celdaBoton.appendChild(Boton);
+//         let Boton = document.createElement('a');
+//         Boton.setAttribute('href','#')
+//         let icono =document.createElement('i');
+//         icono.classList.add("fa-solid")
+//         icono.classList.add("fa-pen-to-square")
+//         icono.classList.add("btnEd")
+//         Boton.appendChild(icono);
+//         celdaBoton.appendChild(Boton);
 
-        let BotonC = document.createElement('a');
-        BotonC.setAttribute('href','#')
-        let iconoC =document.createElement('i');
-        iconoC.classList.add("fa-solid")
-        iconoC.classList.add("fa-circle-xmark")
-        iconoC.classList.add("btnCa")
-        BotonC.appendChild(iconoC);
-        celdaBoton.appendChild(BotonC);
-        }else{
-        let BotonV = document.createElement('a');
-        BotonV.setAttribute('href','#')
-        let iconoV =document.createElement('i');
-        iconoV.classList.add("fa-solid")
-        iconoV.classList.add("fa-eye")
-        iconoV.classList.add("btnV")
-        BotonV.appendChild(iconoV);
-        celdaBoton.appendChild(BotonV);
-        }
+//         let BotonC = document.createElement('a');
+//         BotonC.setAttribute('href','#')
+//         let iconoC =document.createElement('i');
+//         iconoC.classList.add("fa-solid")
+//         iconoC.classList.add("fa-circle-xmark")
+//         iconoC.classList.add("btnCa")
+//         BotonC.appendChild(iconoC);
+//         celdaBoton.appendChild(BotonC);
+//         }else{
+//         let BotonV = document.createElement('a');
+//         BotonV.setAttribute('href','#')
+//         let iconoV =document.createElement('i');
+//         iconoV.classList.add("fa-solid")
+//         iconoV.classList.add("fa-eye")
+//         iconoV.classList.add("btnV")
+//         BotonV.appendChild(iconoV);
+//         celdaBoton.appendChild(BotonV);
+//         }
         
         
-    }
-    let EstadoCita = document.querySelectorAll('.Estado');
-        console.log(EstadoCita.length);
-        VerEstado(EstadoCita);
-}
+//     }
+//     let EstadoCita = document.querySelectorAll('.Estado');
+//         console.log(EstadoCita.length);
+//         VerEstado(EstadoCita);
+// }
 
 
-function VerEstado(EstadoCita){
+ function VerEstado(EstadoCita){
     
-    for (let i = 0; i < EstadoCita.length; i++) {
-    let sEstadoCita = EstadoCita[i].innerHTML;    
-    console.log(sEstadoCita)
-    if (sEstadoCita == 'Pendiente'){
-        EstadoCita[i].classList.add("Pendiente")
+     for (let i = 0; i < EstadoCita.length; i++) {
+     let sEstadoCita = EstadoCita[i].innerHTML;    
+     console.log(sEstadoCita)
+     if (sEstadoCita == 'AGENDADA'){
+         EstadoCita[i].classList.add("AGENDADA")
         
-    }
-    if (sEstadoCita == 'Cancelada'){
-        EstadoCita[i].classList.add("Cancelada")
+     }
+     if (sEstadoCita == 'CANCELADA'){
+         EstadoCita[i].classList.add("CANCELADA")
        
-    }
-    if (sEstadoCita == 'Finalizada'){
-        EstadoCita[i].classList.add("Finalizada")
+     }
+     if (sEstadoCita == 'FINALIZADA'){
+         EstadoCita[i].classList.add("FINALIZADA")
         
-    }   
-    }
-}
+     }   
+     }
+ }
---
+++ public/js/services/CitaService.js
@@ -0,0 +1,55 @@
+"use strict";
+
+let citasArray = []; // arreglo de usuarios
+
+// Esta funcion carga un archivo tipo json y lo carga a un array utilizando un promesa de js
+function cargaJsonCitas() {
+//   console.log("Empezando carga de Mascotas....");
+
+  fetch("/public/assets/data/infoCitas.json")
+    .then((response) => response.json())
+    .then((response) => {
+        citasArray = response;
+        console.log(citasArray);
+    });
+    
+//   return citasArray;
+};
+
+
+function getCitasArray(){
+return citasArray;
+};
+
+// //Esta funcion busca un objeto dentro del arreglo de mascotas segun el campo de nombre y lo devuelve en formato json object.
+// function buscaMascota(pNombreMascota) {
+//   let result = null;
+//   for (let i = 0; i < mascotasArray.length; i++) {
+//     if (mascotasArray[i].NombreMascota.toUpperCase() == pNombreMascota) {
+//       result = mascotasArray[i];
+//     }
+//   }
+//   return result;
+// }
+// //Esta funcion busca un objeto dentro del arreglo de mascotas segun el campo de id y lo devuelve en formato json object.
+// function buscaMascotaPorID(pidMascota) {
+//   let result = null;
+//   for (let i = 0; i < mascotasArray.length; i++) {
+//     if (mascotasArray[i].IdMascota === pidMascota) {
+//       result = mascotasArray[i];
+//     }
+//   }
+//   return result;
+// }
+
+// // Retorna un array con las mascotas de este dueño
+// function buscaMascotasPorDuenio(pIdentificacion) {
+//   let result = [];
+//   for (let i = 0; i < mascotasArray.length; i++) {
+//     if (mascotasArray[i].IdentificacionDuenio === pIdentificacion) {
+//       result.push(mascotasArray[i]);
+//     }
+//   }
+//   return result;
+// }
+
