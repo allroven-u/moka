77ff41173505c420bb320cb5be384b32f8b32ce3
Dns2690 <107601365+Dns2690@users.noreply.github.com>
2022-07-31 01:07:56 -0600

    cambios landing

--- public/js/controllers/controllerLanding.js
+++ public/js/controllers/controllerLanding.js
@@ -29,13 +29,19 @@ window.addEventListener("scroll", function () {
 
 const loginModal = document.querySelector('.lModal-form');
 const signinModal = document.querySelector('.rModal-form');
+const passwordModal = document.querySelector('.cModal-form');
+
 const showModalLogin = document.querySelectorAll(".show-modal-login");
 const showModalSignin = document.querySelectorAll(".show-modal-signin");
+
+
 const closeModalLogin = document.querySelector('.lBtnCancelar');
+const closeModalPassword = document.getElementById('cancelPassword');
+const redirectModalPassword = document.querySelector(".redirect-modal-Password");
 const redirectModalLogin = document.querySelector('.rBtnCancelar');
+const redirectModalSiging = document.querySelector('.redirect-modal-sigin');
 const overlay = document.querySelector('.overlay');
 const general = document.querySelector('body');
-
 
 function disableScroll() {
   window.scrollTo(0, 0);
@@ -59,7 +65,30 @@ for (let i = 0; i < showModalLogin.length; i++) {
     location.href = "#top-page";
     window.addEventListener('scroll', disableScroll);
 
+    // start function show modal
+  function ShowModalSigninFunct() {
+    signinModal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+    location.href = "#top-page";
+    window.addEventListener('scroll', disableScroll);
+  }
+
+  const hiddenModalPassword = function () {
+    passwordModal.classList.add('hidden');
+    overlay.classList.add('hidden');
+    window.removeEventListener('scroll', disableScroll);
+  };
+
+
+  closeModalPassword.addEventListener('click', function () {
+    hiddenModalPassword();
+    ShowModalLoginFunct();
+  });
     closeModalLogin.addEventListener('click', hiddenModalLogin);
+    redirectModalSiging.addEventListener('click', function () {
+      hiddenModalLogin();
+      ShowModalSigninFunct();
+    });
     overlay.addEventListener('click', hiddenModalLogin);
     document.addEventListener('keydown', function (e) {
       if (e.key === 'Escape' && !loginModal.classList.contains('hidden')) {
@@ -90,7 +119,6 @@ for (let i = 0; i < showModalSignin.length; i++) {
     location.href = "#top-page";
     window.addEventListener('scroll', disableScroll);
 
-
     redirectModalLogin.addEventListener('click', function () {
       hiddenModalSignin();
       ShowModalLoginFunct();
@@ -107,6 +135,45 @@ for (let i = 0; i < showModalSignin.length; i++) {
     ShowModalSigninFunct();
   });
 }
+
+///////////////// SHOW AND HIDE MODAL PASSWORD ///////////////////////////////
+
+   const hiddenModallogin = function () {
+    loginModal.classList.add('hidden');
+     overlay.classList.add('hidden');
+     window.removeEventListener('scroll', disableScroll);
+   };
+
+  // start function show modal
+  function ShowModalPasswordFunct() {
+    passwordModal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+    location.href = "#top-page";
+    window.addEventListener('scroll', disableScroll);
+  }
+
+    redirectModalPassword.addEventListener('click', function () {
+      hiddenModallogin();
+      ShowModalPasswordFunct();
+    });
+    overlay.addEventListener('click', hiddenModallogin);
+    document.addEventListener('keydown', function (e) {
+      if (e.key === 'Escape' && !hiddenModallogin.classList.contains('hidden')) {
+        hiddenModallogin();
+      }
+    });
+  
+
+//   showModalSignin[i].addEventListener('click', function () {
+//     ShowModalSigninFunct();
+//   });
+
+
+
+ 
+
+
+
 
 
 
